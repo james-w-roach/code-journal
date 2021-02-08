9b5e65afe0b953707968139d05741862330c301e
James Roach <roachjweston@gmail.com>
2021-02-08 14:50:56 -0800

    added event listener for photo url

--- js/main.js
+++ js/main.js
@@ -1,2 +1,11 @@
 /* global data */
 /* exported data */
+
+var $url = document.querySelector('.image');
+var $input = document.querySelector('#url');
+
+function changeImage(event) {
+  $url.src = $input.value;
+}
+
+$input.addEventListener('input', changeImage);
