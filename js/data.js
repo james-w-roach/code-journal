45da3dcf61db2c7caf48c0c242b01b69a385a546
James Roach <roachjweston@gmail.com>
2021-02-09 12:53:37 -0800

    moved local storage code to data, removed spaces from property names, used reset method

--- js/data.js
+++ js/data.js
@@ -6,3 +6,17 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+
+function beforeUnload(event) {
+  if (Boolean(localStorage.getItem('data')) === true) {
+    var previousJSON = localStorage.getItem('data');
+    var data2 = JSON.parse(previousJSON);
+  } else {
+    data2 = data;
+  }
+
+  var data2JSON = JSON.stringify(data2);
+  localStorage.setItem('data', data2JSON);
+}
+
+window.addEventListener('beforeunload', beforeUnload);
--- js/main.js
+++ js/main.js
@@ -20,28 +20,15 @@ var $notesInput = document.querySelector('#notes');
 
 function onSubmit(event) {
   var object = {};
-  object['image URL'] = $urlInput.value;
-  object['entry title'] = $titleInput.value;
-  object['entry notes'] = $notesInput.value;
+  object.imageURL = $urlInput.value;
+  object.entryTitle = $titleInput.value;
+  object.entryNotes = $notesInput.value;
 
-  if (Boolean(localStorage.getItem('data')) === true) {
-    var previousJSON = localStorage.getItem('data');
-    var data2 = JSON.parse(previousJSON);
-  } else {
-    data2 = data;
-  }
+  object.nextEntryId = data.nextEntryId;
+  data.nextEntryId++;
+  data.entries.unshift(object);
 
-  object.nextEntryId = data2.nextEntryId;
-  data2.nextEntryId++;
-  data2.entries.unshift(object);
-
-  var data2JSON = JSON.stringify(data2);
-  localStorage.setItem('data', data2JSON);
-
-  $url.src = 'images/placeholder-image-square.jpg';
-  $titleInput.value = '';
-  $notesInput.value = '';
-  $urlInput.value = '';
+  $form.reset();
 }
 
 $form.addEventListener('submit', onSubmit);
