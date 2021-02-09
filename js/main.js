073be2bd6e726bd04da7d1da412902dbb6d9afd9
James Roach <roachjweston@gmail.com>
2021-02-08 17:15:50 -0800

    added event listener to form

--- js/main.js
+++ js/main.js
@@ -9,3 +9,23 @@ function changeImage(event) {
 }
 
 $input.addEventListener('input', changeImage);
+
+var $form = document.querySelector('.form');
+var $titleInput = document.querySelector('#title');
+var $notesInput = document.querySelector('#notes');
+
+function onSubmit(event) {
+  var object = {};
+  object['image URL'] = $input.value;
+  object['entry title'] = $titleInput.value;
+  object.notes = $notesInput.value;
+  object.nextEntryId = data.nextEntryId;
+  data.nextEntryId++;
+  data.entries.unshift(object);
+  $url.src = 'images/placeholder-image-square.jpg';
+  $titleInput.value = '';
+  $notesInput.value = '';
+  $input.value = '';
+}
+
+$form.addEventListener('submit', onSubmit);
