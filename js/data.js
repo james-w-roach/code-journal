4c2c01ec20d26459dc94891e995f90262b2b41d7
James Roach <roachjweston@gmail.com>
2021-02-10 17:56:36 -0800

    ensured refresh doesn't change view

--- js/data.js
+++ js/data.js
@@ -13,9 +13,27 @@ if (previousJSON) {
   data = JSON.parse(previousJSON);
 }
 
+var $form = document.querySelector('.form');
+var $entriesSection = document.querySelector('.entry-page');
+var formView = localStorage.getItem('formView');
+var entriesView = localStorage.getItem('entriesView');
+
+if (entriesView) {
+  $entriesSection.className = JSON.parse(entriesView);
+}
+
+if (formView) {
+  $form.className = JSON.parse(formView);
+}
+
 function beforeUnload(event) {
   var dataJSON = JSON.stringify(data);
   localStorage.setItem('data', dataJSON);
+
+  var classStringifyEntries = JSON.stringify($entriesSection.className);
+  var classStringifyForm = JSON.stringify($form.className);
+  localStorage.setItem('formView', classStringifyForm);
+  localStorage.setItem('entriesView', classStringifyEntries);
 }
 
 window.addEventListener('beforeunload', beforeUnload);
